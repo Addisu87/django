0bdf16f3027fbde4ecd5a1172bd3d64b6f6824ee
Addisu87 <addisuhaile87@gmail.com>
2021-11-13 20:24:55 +0300

    feed change

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView, DetailView
+from django.contrib import messages
+from django.views.generic import TemplateView, DetailView, FormView
 
+from .forms import PostForm
 from .models import Post
 
 class HomePageView(TemplateView):
@@ -7,9 +9,22 @@ class HomePageView(TemplateView):
   
   def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
-      context["posts"] = Post.objects.all()
+      context["posts"] = Post.objects.all().order_by('-id')
       return context
   
 class PostDetailView(DetailView):
   template_name = "detail.html"
   model = Post
+  
+class  AddPostView(FormView):
+  template_name = "new_post.html"
+  form_class = PostForm
+  success_url = "/"
+  
+  def form_valid(self, form):
+        # Create a new post 
+        new_object = Post.objects.create(
+          text=form.cleaned_data['text'],
+          image=form.cleaned_data['image']
+        )
+        return super().form_valid(form)
